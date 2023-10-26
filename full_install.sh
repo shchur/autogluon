f74703228534113aa807fa85d73521b89fe749ce
Oleksandr Shchur <shchuro@amazon.com>
2023-10-26 11:44:26 +0000

    Limited install for benchmark

--- full_install.sh
+++ full_install.sh
@@ -4,7 +4,5 @@ python3 -m pip install -e common/[tests]
 python3 -m pip install -e core/[all,tests]
 python3 -m pip install -e features/
 python3 -m pip install -e tabular/[all,tests]
-python3 -m pip install -e multimodal/[tests]
 python3 -m pip install -e timeseries/[all,tests]
-python3 -m pip install -e eda/
 python3 -m pip install -e autogluon/
